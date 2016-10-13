1fc4fb1fbb275e9cf48ee5bbaec8cc3bc71dd25a
julien <julien.paille75@gmail.com>
2016-10-13 14:19:25 +0200

    Set default test settings to feedback.envs.test_offline.
    `test_offline` settings are used to avoid api call while testing.

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "feedback.settings")
-
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          'feedback.envs.test_offline' if 'test' in sys.argv else 'feedback.settings')
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
